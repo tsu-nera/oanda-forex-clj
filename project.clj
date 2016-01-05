b925fe07ef54aa65d46db84f0adb06938aa38a04
tsu-nera <fox10225fox@gmail.com>
2016-01-05 16:59:07 +0900

    諦め

--- project.clj
+++ project.clj
@@ -8,6 +8,6 @@
                  [incanter "1.5.6"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-time "0.10.0"]
-                 [clj-http "2.0.0"]]
+                 [clj-http "2.0.0"]
+                 [clj-json "0.5.3"]]
   :main fx-clj.core)
-  
